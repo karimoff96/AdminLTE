fc45f37cb9ec41eb41b726bca3b4fd0786554936
doniyor <doniyorkarimoff96@gmail.com>
2022-01-05 19:28:28 +0500

    minor changes

--- NewProject/settings.py
+++ NewProject/settings.py
@@ -113,18 +113,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"), '/static/'
+    os.path.join(BASE_DIR, "static"), '/static/'
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn crud.wsgi --log-file -
+web: gunicorn NewProject.wsgi --log-file -
